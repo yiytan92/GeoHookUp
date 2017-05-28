94324ee57c882e42e9289b7da5eb28a84087b742
Yi Yang Tan <yiytan@usc.edu>
2017-05-27 23:04:42 -0700

    3rd update

--- GeoHookUp/UserLocationController.swift
+++ GeoHookUp/UserLocationController.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+import CoreLocation
+
+class UserLocationController:NSObject,CLLocationManagerDelegate{
+    
+    static let sharedLocationController:UserLocationController = UserLocationController()
+    static var currentLocation:CLLocation? = nil
+    static let locationManager:CLLocationManager = CLLocationManager()
+    
+    class func startGPS(){
+        locationManager.delegate = sharedLocationController
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.requestAlwaysAuthorization()
+
+        if (CLLocationManager.locationServicesEnabled()){
+        locationManager.startUpdatingLocation()
+        }
+        
+        
+    }
+    
+    class func stopGPS(){
+        locationManager.stopUpdatingLocation()    }
+    
+    class func deviceLocation() -> CLLocationCoordinate2D{
+            return (currentLocation?.coordinate)!
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        UserLocationController.currentLocation = locations[0]
+        
+    }
+}
--- GeoHookUp/ViewController.swift
+++ GeoHookUp/ViewController.swift
@@ -7,14 +7,79 @@
 //
 
 import UIKit
+import MapKit
+import Foundation
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBOutlet weak var userLatitude: UILabel!
+    
+    @IBOutlet weak var userLongitude: UILabel!
+    
+    @IBAction func updateLocation(_ sender: Any) {
+    }
+    var locationManager = CLLocationManager()
+    
+    var uniqueString:String = UUID().uuidString
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        print("This is the generated String: \(String(describing:uniqueString))")
+    
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
+        if (CLLocationManager.locationServicesEnabled()) {
+            locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestAlwaysAuthorization()
+            locationManager.requestWhenInUseAuthorization()
+        }
+        locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
+        //Zoom to user location
+        //set initial zone at USC
+        let initialLocation = CLLocation(latitude: 32.8801, longitude: -117.2340)
+        centerMapOnLocation(location: initialLocation)
+       
 
+    
+    }
+    
+
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation:CLLocation = locations[0]
+        let long = userLocation.coordinate.longitude;
+        let lat = userLocation.coordinate.latitude;
+        userLatitude.text = String(lat)
+        userLongitude.text = String(long)
+        
+        //Do What ever you want with it
+    }
+    
+    let regionRadius: CLLocationDistance = 3000
+
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
